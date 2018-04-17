d22e92dfbaff3a49213ed94d338e473dfeb6e979
Matthew Hoover <MatthewHoover@Knights.ucf.edu>
2018-04-17 17:55:08 -0400

    Like Library Images
    Click on an image in the library, it gets big, and there's a button you can click to like it. Honestly, the button does nothing, but it looks important.

--- app/src/main/java/com/marshmellowman/collageucf/Library.java
+++ app/src/main/java/com/marshmellowman/collageucf/Library.java
@@ -11,10 +11,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
@@ -45,15 +49,43 @@ public class Library extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View thisView = inflater.inflate(R.layout.fragment_library, container, false);
+        final View thisView = inflater.inflate(R.layout.fragment_library, container, false);
+        final ImageView mainImage = thisView.findViewById(R.id.library_image);
+        final LinearLayout holder = thisView.findViewById(R.id.library_image_holder);
 
         dynamoDBMapper = AppInfo.getInstance().getDynamoDBMapper();
 
+        holder.setVisibility(View.GONE);
+        mainImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.setVisibility(View.GONE);
+                ((Button) thisView.findViewById(R.id.library_like)).setText("Like");
+            }
+        });
+        ((Button) thisView.findViewById(R.id.library_like)).setOnClickListener(
+            new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   ((Button) v).setText("Liked");
+               }
+           }
+
+        );
+
         // Attempt at a grid of pictures
-        ArrayList<Bitmap> array = new ArrayList<>();
+        ArrayList<ListItem> array = new ArrayList<>();
         GridAdapter adapter = new GridAdapter(thisView.getContext(), R.layout.library_item, array);
         GridView listView = (GridView) thisView.findViewById(R.id.library_grid);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mainImage.setImageBitmap(((ListItem) parent.getItemAtPosition(position)).bitmap);
+                holder.setVisibility(View.VISIBLE);
+                ((TextView) thisView.findViewById(R.id.library_user)).setText(((ListItem) parent.getItemAtPosition(position)).string);
+            }
+        });
 
         // Create Handler to connect alt Thread (where DB connections can happen) to the Main Thread (where UI changes can happen).
         // Pass it this view so you can change it below.
@@ -82,16 +114,13 @@ public class Library extends Fragment {
                     for (PostDBDO p : list) {
                         msg = handler.obtainMessage();
                         msg.what = S3_IMAGE;
-                        msg.obj = BitmapFactory.decodeStream(new java.net.URL(uri + p.getImageURL()).openStream());
+                        msg.obj = new ListItem(BitmapFactory.decodeStream(new java.net.URL(uri + p.getImageURL()).openStream())
+                                , p.getUploader());
                         handler.sendMessage(msg);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                msg = handler.obtainMessage();
-                msg.what = DB_TEXT;
-                msg.obj = "Loaded!";
-                handler.sendMessage(msg);
             }
         });
         t2.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
@@ -124,14 +153,14 @@ public class Library extends Fragment {
 
         public void handleMessage(Message msg) {
             if(msg.what==DB_TEXT){
-                ((TextView) thisView.findViewById(R.id.library_list)).setText( (String) msg.obj );
+                Toast.makeText(thisView.getContext(), (String) msg.obj, Toast.LENGTH_SHORT).show();
             }
             else if (msg.what == S3_IMAGE) {
-                array.add((Bitmap) msg.obj);
+                array.add(msg.obj);
                 adapter.notifyDataSetChanged();
             }
             else if (msg.what == DB_ERROR) {
-                ((TextView) thisView.findViewById(R.id.library_list)).setText( "Problems!" + ((Throwable) msg.obj).getLocalizedMessage() );
+                Toast.makeText(thisView.getContext(), "Problems!" + ((Throwable) msg.obj).getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
             super.handleMessage(msg);
         }
@@ -164,8 +193,18 @@ public class Library extends Fragment {
                 currentView = (View) convertView;
             }
 
-            ( (ImageView) currentView.findViewById(R.id.thumbnail)).setImageBitmap( (Bitmap) list.get(position) );
+            ( (ImageView) currentView.findViewById(R.id.thumbnail)).setImageBitmap( (Bitmap) ((ListItem)list.get(position)).bitmap );
             return currentView;
+        }
+    }
+
+    private class ListItem {
+        public Bitmap bitmap;
+        public String string;
+
+        public ListItem(Bitmap bitmap, String string) {
+            this.bitmap = bitmap;
+            this.string = string;
         }
     }
 }
